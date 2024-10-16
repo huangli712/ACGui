b7a9d50fb947506b0a500456622804009a67c053
Li Huang <lihuang.dmft@gmail.com>
2024-10-16 20:11:04 +0800

    acg.jl

--- util/acg.jl
+++ util/acg.jl
@@ -7,6 +7,7 @@ function acg_layout!(app::Dash.DashApp)
         html_h2("ACGui: A Graphic User Interface For ACFlow"),
         html_h4("Developed by Li Huang (hungli@caep.cn)"),
         layout_base_block(),
+        html_br(),
         layout_maxent_block()
     end
 end
@@ -15,7 +16,7 @@ function layout_base_block()
     html_table([
         html_thead(
             html_tr(
-                html_th(html_label("Base block"), colSpan = 3)
+                html_th(html_label("[Base] block"), colSpan = 3)
             )
         ),
         #
@@ -98,79 +99,39 @@ function layout_maxent_block()
     html_table([
         html_thead(
             html_tr(
-                html_th(html_label("Base block"), colSpan = 3)
+                html_th(html_label("[MaxEnt] block"), colSpan = 3)
             )
         ),
         #
         html_tbody([
             html_tr([
-                html_th(html_label("Filename for input data")),
-                html_td(html_label("finput")),
+                html_th(html_label("How to determine the optimized α parameter")),
+                html_td(html_label("method")),
                 html_td(dcc_input(type = "text", placeholder = "input")),
             ]),
             html_tr([
-                html_th(html_label("Solver for the analytic continuation problem")),
-                html_td(html_label("solver")),
+                html_th(html_label("Type of the entropy term")),
+                html_td(html_label("stype")),
                 html_td(dcc_input(type = "text", placeholder = "input")),
             ]),
             html_tr([
-                html_th(html_label("Type of kernel function")),
-                html_td(html_label("ktype")),
+                html_th(html_label("Total number of the chosen α parameters")),
+                html_td(html_label("nalph")),
                 html_td(dcc_input(type = "text", placeholder = "input")),
             ]),
             html_tr([
-                html_th(html_label("Type of default model function")),
-                html_td(html_label("mtype")),
+                html_th(html_label("Starting value for the α parameter")),
+                html_td(html_label("alpha")),
                 html_td(dcc_input(type = "text", placeholder = "input")),
             ]),
             html_tr([
-                html_th(html_label("Grid for input data (imaginary axis)")),
-                html_td(html_label("grid")),
+                html_th(html_label("Scaling factor for the α parameter")),
+                html_td(html_label("ratio")),
                 html_td(dcc_input(type = "text", placeholder = "input")),
             ]),
             html_tr([
-                html_th(html_label("Mesh for output data (real axis)")),
-                html_td(html_label("mesh")),
-                html_td(dcc_input(type = "text", placeholder = "input")),
-            ]),
-            html_tr([
-                html_th(html_label("Number of grid points")),
-                html_td(html_label("ngrid")),
-                html_td(dcc_input(type = "text", placeholder = "input")),
-            ]),
-            html_tr([
-                html_th(html_label("Number of mesh points")),
-                html_td(html_label("nmesh")),
-                html_td(dcc_input(type = "text", placeholder = "input")),
-            ]),
-            html_tr([
-                html_th(html_label("Right boundary (maximum value) of output mesh")),
-                html_td(html_label("wmax")),
-                html_td(dcc_input(type = "text", placeholder = "input")),
-            ]),
-            html_tr([
-                html_th(html_label("Left boundary (minimum value) of output mesh")),
-                html_td(html_label("wmin")),
-                html_td(dcc_input(type = "text", placeholder = "input")),
-            ]),
-            html_tr([
-                html_th(html_label("Inverse temperature")),
-                html_td(html_label("beta")),
-                html_td(dcc_input(type = "text", placeholder = "input")),
-            ]),
-            html_tr([
-                html_th(html_label("Is it the offdiagonal part in matrix-valued function")),
-                html_td(html_label("offdiag")),
-                html_td(dcc_input(type = "text", placeholder = "input")),
-            ]),
-            html_tr([
-                html_th(html_label("Are the analytic continuation results written into files")),
-                html_td(html_label("fwrite")),
-                html_td(dcc_input(type = "text", placeholder = "input")),
-            ]),
-            html_tr([
-                html_th(html_label("Restriction of the energy range of the spectrum")),
-                html_td(html_label("exclude")),
+                html_th(html_label("Shall we preblur the kernel and spectrum")),
+                html_td(html_label("blur")),
                 html_td(dcc_input(type = "text", placeholder = "input")),
             ]),
         ]),
