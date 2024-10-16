36a952762a24df1a1205c1429ef9f44c01a22570
Li Huang <lihuang.dmft@gmail.com>
2024-10-16 18:31:28 +0800

    update acg.jl

--- util/acg.jl
+++ util/acg.jl
@@ -12,45 +12,86 @@ function acg_layout!(app::Dash.DashApp)
 end
 
 function layout_base_block()
-    html_table(
+    html_table([
+        html_thead(
+            html_tr(
+                html_th(html_label("Base block"), colSpan = 3)
+            )
+        ),
+        #
         html_tbody([
             html_tr([
-                html_th(html_label("File name for input data")),
+                html_th(html_label("Filename for input data")),
                 html_td(html_label("finput")),
                 html_td(dcc_input(type = "text", placeholder = "input")),
             ]),
             html_tr([
-                html_th(html_label("File name for input data")),
-                html_td(html_label("finput")),
+                html_th(html_label("Solver for the analytic continuation problem")),
+                html_td(html_label("solver")),
                 html_td(dcc_input(type = "text", placeholder = "input")),
             ]),
             html_tr([
-                html_th(html_label("File name for input data")),
-                html_td(html_label("finput")),
+                html_th(html_label("Type of kernel function")),
+                html_td(html_label("ktype")),
                 html_td(dcc_input(type = "text", placeholder = "input")),
             ]),
             html_tr([
-                html_th(html_label("File name for input data")),
-                html_td(html_label("finput")),
+                html_th(html_label("Type of default model function")),
+                html_td(html_label("mtype")),
                 html_td(dcc_input(type = "text", placeholder = "input")),
             ]),
             html_tr([
-                html_th(html_label("File name for input data")),
-                html_td(html_label("finput")),
+                html_th(html_label("Grid for input data (imaginary axis)")),
+                html_td(html_label("grid")),
                 html_td(dcc_input(type = "text", placeholder = "input")),
             ]),
             html_tr([
-                html_th(html_label("File name for input data")),
-                html_td(html_label("finput")),
+                html_th(html_label("Mesh for output data (real axis)")),
+                html_td(html_label("mesh")),
                 html_td(dcc_input(type = "text", placeholder = "input")),
             ]),
             html_tr([
-                html_th(html_label("File name for input data")),
-                html_td(html_label("finput")),
+                html_th(html_label("Number of grid points")),
+                html_td(html_label("ngrid")),
                 html_td(dcc_input(type = "text", placeholder = "input")),
             ]),
-        ])
-    )
+            html_tr([
+                html_th(html_label("Number of mesh points")),
+                html_td(html_label("nmesh")),
+                html_td(dcc_input(type = "text", placeholder = "input")),
+            ]),
+            html_tr([
+                html_th(html_label("Right boundary (maximum value) of output mesh")),
+                html_td(html_label("wmax")),
+                html_td(dcc_input(type = "text", placeholder = "input")),
+            ]),
+            html_tr([
+                html_th(html_label("Left boundary (minimum value) of output mesh")),
+                html_td(html_label("wmin")),
+                html_td(dcc_input(type = "text", placeholder = "input")),
+            ]),
+            html_tr([
+                html_th(html_label("Inverse temperature")),
+                html_td(html_label("beta")),
+                html_td(dcc_input(type = "text", placeholder = "input")),
+            ]),
+            html_tr([
+                html_th(html_label("Is it the offdiagonal part in matrix-valued function")),
+                html_td(html_label("offdiag")),
+                html_td(dcc_input(type = "text", placeholder = "input")),
+            ]),
+            html_tr([
+                html_th(html_label("Are the analytic continuation results written into files")),
+                html_td(html_label("fwrite")),
+                html_td(dcc_input(type = "text", placeholder = "input")),
+            ]),
+            html_tr([
+                html_th(html_label("Restriction of the energy range of the spectrum")),
+                html_td(html_label("exclude")),
+                html_td(dcc_input(type = "text", placeholder = "input")),
+            ]),
+        ]),
+    ])
 end
 
 function layout_maxent_block()
