6d07353054fa9bf6a152f4bb2f53fff09ee88e2a
Li Huang <lihuang.dmft@gmail.com>
2024-10-16 17:49:52 +0800

    fix acg.jl

--- util/acg.jl
+++ util/acg.jl
@@ -12,58 +12,22 @@ function acg_layout!(app::Dash.DashApp)
 end
 
 function layout_base_block()
-    html_div(
-        children = [
-            html_label("[BASE] block"),
-            html_br(),
-            html_label("finput : "),
-            dcc_input(type = "text", placeholder = "input"),
-            html_br(),
-            html_br(),
-            html_label("solver : "),
-            dcc_input(type = "text", placeholder = "input"),
-            html_br(),
-            html_label("ktype : "),
-            dcc_input(type = "text", placeholder = "input"),
-            html_br(),
-            html_label("mtype : "),
-            dcc_input(type = "text", placeholder = "input"),
-            html_br(),
-            html_label("grid : "),
-            dcc_input(type = "text", placeholder = "input"),
-            html_br(),
-            html_label("mesh : "),
-            dcc_input(type = "text", placeholder = "input"),
-            html_br(),
-            html_label("ngrid : "),
-            dcc_input(type = "text", placeholder = "input"),
-            html_br(),
-            html_label("nmesh : "),
-            dcc_input(type = "text", placeholder = "input"),
-            html_br(),
-            html_label("wmax : "),
-            dcc_input(type = "text", placeholder = "input"),
-            html_br(),
-            html_label("wmin : "),
-            dcc_input(type = "text", placeholder = "input"),
-            html_br(),
-            html_label("beta : "),
-            dcc_input(type = "text", placeholder = "input"),
-            html_br(),
-            html_label("offdiag : "),
-            dcc_input(type = "text", placeholder = "input"),
-            html_br(),
-            html_label("fwrite : "),
-            dcc_input(type = "text", placeholder = "input"),
-        ],
+    html_table(
+        html_tbody([
+            html_tr([
+                html_th(html_label("finput : ")),
+                html_td(dcc_input(type = "text", placeholder = "input")),
+            ]), 
+        ])
     )
 end
 
 function layout_maxent_block()
     html_div(
         children = [
+            html_label("finput : "),
             dcc_input(type = "text", placeholder = "input"),
-            dcc_input(type = "text", placeholder = "input"),
+            html_label("solver : "),
             dcc_input(type = "text", placeholder = "input"),
         ],
     )
