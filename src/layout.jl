41f979d5b105265a833cefc44815b440bc424ccb
Li Huang <lihuang.dmft@gmail.com>
2024-10-25 00:20:03 +0800

    fix layout.jl

--- src/layout.jl
+++ src/layout.jl
@@ -98,3 +98,202 @@ function layout_data_block()
         ),
     ])
 end
+
+function layout_base_block()
+    html_table([
+        html_thead(
+            html_tr(
+                html_th(
+                    html_label("Configuration parameters: general setup"),
+                    colSpan = 3
+                )
+            )
+        ),
+        #
+        html_tbody([
+            html_tr([
+                html_th(html_label("Filename for input data")),
+                html_td(html_label("finput")),
+                html_td(
+                    dcc_input(
+                        id = "base-finput",
+                        type = "text",
+                        value = "giw.data",
+                        readOnly = true
+                    )
+                ),
+            ]),
+            html_tr([
+                html_th(html_label("Solver for the analytic continuation problem")),
+                html_td(html_label("solver")),
+                html_td(
+                    dcc_dropdown(
+                        id = "base-solver",
+                        options = [
+                            (label = "MaxEnt", value = "MaxEnt"),
+                            (label = "BarRat", value = "BarRat"),
+                            (label = "StochPX", value = "StochPX"),
+                        ],
+                        value = "MaxEnt",
+                    )
+                ),
+            ]),
+            html_tr([
+                html_th(html_label("Type of kernel function")),
+                html_td(html_label("ktype")),
+                html_td(
+                    dcc_dropdown(
+                        id = "base-ktype",
+                        options = [
+                            (label = "fermi", value = "fermi"),
+                            (label = "boson", value = "boson"),
+                            (label = "bsymm", value = "bsymm"),
+                        ],
+                        value = "fermi",
+                    )
+                ),
+            ]),
+            html_tr([
+                html_th(html_label("Type of default model function")),
+                html_td(html_label("mtype")),
+                html_td(
+                    dcc_dropdown(
+                        id = "base-mtype",
+                        options = [
+                            (label = "flat", value = "flat"),
+                            (label = "gauss", value = "gauss"),
+                            (label = "1gauss", value = "1gauss"),
+                            (label = "2gauss", value = "2gauss"),
+                            (label = "lorentz", value = "lorentz"),
+                            (label = "1lorentz", value = "1lorentz"),
+                            (label = "2lorentz", value = "2lorentz"),
+                            (label = "risedecay", value = "risedecay"),
+                        ],
+                        value = "flat",
+                    )
+                ),
+            ]),
+            html_tr([
+                html_th(html_label("Grid for input data (imaginary axis)")),
+                html_td(html_label("grid")),
+                html_td(
+                    dcc_dropdown(
+                        id = "base-grid",
+                        options = [
+                            (label = "ftime", value = "ftime"),
+                            (label = "fpart", value = "fpart"),
+                            (label = "btime", value = "btime"),
+                            (label = "bpart", value = "bpart"),
+                            (label = "ffreq", value = "ffreq"),
+                            (label = "ffrag", value = "ffrag"),
+                            (label = "bfreq", value = "bfreq"),
+                            (label = "bfrag", value = "bfrag"),
+                        ],
+                        value = "ftime",
+                    )
+                ),
+            ]),
+            html_tr([
+                html_th(html_label("Mesh for output data (real axis)")),
+                html_td(html_label("mesh")),
+                html_td(
+                    dcc_dropdown(
+                        id = "base-mesh",
+                        options = [
+                            (label = "linear", value = "linear"),
+                            (label = "tangent", value = "tangent"),
+                            (label = "lorentz", value = "lorentz"),
+                            (label = "halflorentz", value = "halflorentz"),
+                        ],
+                        value = "linear",
+                    )
+                ),
+            ]),
+            html_tr([
+                html_th(html_label("Number of grid points")),
+                html_td(html_label("ngrid")),
+                html_td(
+                    dcc_input(
+                        id = "base-ngrid",
+                        type = "text",
+                        value = "10"
+                    )
+                ),
+            ]),
+            html_tr([
+                html_th(html_label("Number of mesh points")),
+                html_td(html_label("nmesh")),
+                html_td(
+                    dcc_input(
+                        id = "base-nmesh",
+                        type = "text",
+                        value = "501"
+                    )
+                ),
+            ]),
+            html_tr([
+                html_th(html_label("Right boundary (maximum value) of output mesh")),
+                html_td(html_label("wmax")),
+                html_td(
+                    dcc_input(
+                        id = "base-wmax",
+                        type = "text",
+                        value = "5.0"
+                    )
+                ),
+            ]),
+            html_tr([
+                html_th(html_label("Left boundary (minimum value) of output mesh")),
+                html_td(html_label("wmin")),
+                html_td(
+                    dcc_input(
+                        id = "base-wmin",
+                        type = "text",
+                        value = "-5.0"
+                    )
+                ),
+            ]),
+            html_tr([
+                html_th(html_label("Inverse temperature")),
+                html_td(html_label("beta")),
+                html_td(
+                    dcc_input(
+                        id = "base-beta",
+                        type = "text",
+                        value = "10.0"
+                    )
+                ),
+            ]),
+            html_tr([
+                html_th(html_label("Is it the offdiagonal part in matrix-valued function")),
+                html_td(html_label("offdiag")),
+                html_td(
+                    dcc_radioitems(
+                        id = "base-offdiag",
+                        options = [
+                            (label = "Yes", value = "true"),
+                            (label = "No", value = "false"),
+                        ],
+                        value = "false",
+                        labelStyle = Dict("display" => "inline-block")
+                    )
+                ),
+            ]),
+            html_tr([
+                html_th(html_label("Are the analytic continuation results written into files")),
+                html_td(html_label("fwrite")),
+                html_td(
+                    dcc_radioitems(
+                        id = "base-fwrite",
+                        options = [
+                            (label = "Yes", value = "true"),
+                            (label = "No", value = "false"),
+                        ],
+                        value = "false",
+                        labelStyle = Dict("display" => "inline-block")
+                    )
+                ),
+            ]),
+        ]),
+    ])
+end
