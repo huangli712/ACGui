1aac9627b6eaca3767659fb26161d26392edf1f8
Li Huang <lihuang.dmft@gmail.com>
2024-10-25 00:17:00 +0800

    fix layout.jl

--- src/layout.jl
+++ src/layout.jl
@@ -4,12 +4,47 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2024/10/16
+# Last modified: 2024/10/25
 #
 
 function acg_layout!(app::Dash.DashApp)
     app.layout = html_div() do 
-        html_h1("ACGui: A Graphic User Interface For ACFlow"),
-        html_h2("Develop")
+        layout_header_block(),
+        html_br(),
+        dcc_tabs(children = [
+            dcc_tab(
+                label = "Data",
+                children = layout_data_block(),
+                className = "custom-tab",
+            ),
+            dcc_tab(
+                label = "General",
+                children = layout_base_block(),
+                className = "custom-tab",
+            ),
+            dcc_tab(
+                label = "Solver",
+                children = [
+                    layout_maxent_block(),
+                    layout_barrat_block(),
+                    layout_stochpx_block(),
+                ],
+                className = "custom-tab",
+            ),
+            dcc_tab(
+                label = "Run",
+                children = layout_calc_block(),
+                className = "custom-tab",
+            ),
+            dcc_tab(
+                label = "About",
+                children = layout_about_block(),
+                className = "custom-tab",
+            ),
+        ]),
+        html_br(),
+        html_br(),
+        html_br()
     end
 end
+
