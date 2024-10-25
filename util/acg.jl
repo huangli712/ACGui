c1ce31873bea57270dd19b87d13ff0bde4d2474a
Li Huang <lihuang.dmft@gmail.com>
2024-10-25 11:06:54 +0800

    fix comments in acg.jl

--- util/acg.jl
+++ util/acg.jl
@@ -1,5 +1,22 @@
 #!/usr/bin/env julia
 
+#
+# This julia script will start a web app. Then the users can visit this
+# app with any modern web browser, such as Chrome, Edge, and Safari. The
+# default URL is
+#
+#    http://0.0.0.0:8848
+#
+# This app provides a simple yet userful graphic user interface for the
+# `ACFlow` package. The users can use it to do analytic continuations
+# and visualize the calculated results online. Note that this app suits
+# short analytic continuation simulations.
+#
+# Usage:
+#
+#     $ acg.jl
+#
+
 push!(LOAD_PATH,"/Users/lihuang/Working/devel/ACGui/src")
 push!(LOAD_PATH,"/Users/lihuang/Working/devel/ACFlow/src")
 
