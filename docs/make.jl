36c91c5e12b037c3fa5428b015024e559650a2e0
Li Huang <lihuang.dmft@gmail.com>
2024-10-26 17:19:24 +0800

    add library.md

---
+++ docs/make.jl
@@ -0,0 +1,25 @@
+push!(LOAD_PATH, "../src")
+push!(LOAD_PATH, "../../ACFlow/src")
+
+using Documenter
+using Random
+using ACGui
+
+makedocs(
+    sitename = "ACGui",
+    clean = false,
+    authors = "Li Huang <huangli@caep.cn> and contributors",
+    format = Documenter.HTML(
+        prettyurls = false,
+        ansicolor = true,
+        repolink = "https://github.com/huangli712/ACGui",
+        size_threshold = 409600, # 400kb
+    ),
+    #format = Documenter.LaTeX(platform = "none"),
+    remotes = nothing,
+    modules = [ACGui],
+    pages = [
+        "Home" => "index.md",
+        "Library" => "library.md",
+    ],
+)
