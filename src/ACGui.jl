d7c866672f50d71cecc0136a3ed84633b6ce0ef0
Li Huang <lihuang.dmft@gmail.com>
2024-10-26 09:17:30 +0800

    fix ACGui.jl

--- src/ACGui.jl
+++ src/ACGui.jl
@@ -4,9 +4,26 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2024/10/25
+# Last modified: 2024/10/26
 #
 
+"""
+    ACGui
+
+ACGui is a simple web app for the `ACFlow` package. It depends on the Dash
+web framework, and provide an useful ui to facilite analytic continuation
+calculations. Now ACGui supports three analytic continuation solvers:
+
+* Maximum Entropy Method (`MaxEnt` solver, `recommended`)
+* Barycentric Rational Function Method (`BarRat` solver, `recommended`)
+* Stochastic Pole eXpansion (`StochPX` solver, `recommended`)
+
+The MaxEnt and BarRat solvers are extremely fast, so users can obtain the
+calculated results quickly. However, the StochPX solver is quite slow. It
+is not a good idea to start calculations with it through ACGui. In such
+cases, users can download the relevant `ac.toml` files from the app, and
+then submit tasks manually.
+"""
 module ACGui
 
 using Dash
