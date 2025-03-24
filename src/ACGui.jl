bca9dc079964dca84740c288b32b4ef5a3baf5ea
Li Huang <lihuang.dmft@gmail.com>
2025-03-24 16:19:27 +0800

    add new layout functions

--- src/ACGui.jl
+++ src/ACGui.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2025/02/18
+# Last modified: 2025/03/24
 #
 
 """
@@ -12,15 +12,17 @@
 
 ACGui is a simple web app for the `ACFlow` package. It depends on the Dash
 web framework, and provide an useful ui to facilite analytic continuation
-calculations. Now ACGui supports four analytic continuation solvers:
+calculations. Now ACGui supports six analytic continuation solvers:
 
 * Maximum Entropy Method (`MaxEnt` solver, `recommended`)
 * Barycentric Rational Function Method (`BarRat` solver, `recommended`)
 * Stochastic Analytic Continuation (`StochAC` solver, Beach's algorithm)
+* Stochastic Analytic Continuation (`StochSK` solver, Sandvik's algorithm)
+* Stochastic Optimization Method (`StochOM` solver)
 * Stochastic Pole eXpansion (`StochPX` solver, `recommended`)
 
 The MaxEnt and BarRat solvers are extremely fast, so users can obtain the
-calculated results quickly. However, the StochAC and StochPX solvers are
+calculated results quickly. However, the other stochastic solvers are
 quite slow (they could spend several hours solving analytic continuation
 problems). It is not a good idea to start calculations with them through
 ACGui. In such cases, users can download the relevant `ac.toml` files from
@@ -43,6 +45,8 @@ export layout_base_block
 export layout_maxent_block
 export layout_barrat_block
 export layout_stochac_block
+export layout_stochsk_block
+export layout_stochom_block
 export layout_stochpx_block
 export layout_calc_block
 export layout_about_block
